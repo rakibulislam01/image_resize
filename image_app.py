b3e18d27307d9ab2749517261560bc23013671fd
rakibulislam01 <mrakibul70@gmail.com>
2019-07-24 17:26:55 +0600

    Update GUI.

--- image_app.py
+++ image_app.py
@@ -1,7 +1,74 @@
 import tkinter as tk
+import tkinter
+import tkinter.filedialog
+from image_resize import image_location
 
-r = tk.Tk()
-r.title('Counting Seconds')
-button = tk.Button(r, text='Stop', width=25, command=r.destroy)
-button.pack()
-r.mainloop()
+master = tk.Tk()
+e1 = tk.Entry(master)
+e2 = tk.Entry(master)
+tk.Label(master,
+         text="First Name").grid(row=0)
+tk.Label(master,
+         text="Last Name").grid(row=1)
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+
+
+def call_function_(f, first, second):
+    image_location(f, first, second)
+
+
+def print_path(first, second):
+    f = tkinter.filedialog.askopenfilename(
+        parent=master, initialdir='/home/rakibul/Desktop/',
+        title='Choose file',
+        filetypes=[('png images', '.png'),
+                   ('jpag images', '.jpg'),
+                   ('gif images', '.gif')]
+    )
+    print(f)
+    image_location(f, first, second)
+    return f
+
+
+def button_value():
+    first = e1.get()
+    second = e2.get()
+
+    print_path(first, second)
+    return first, second
+
+
+tk.Button(master,
+          text='Quit',
+          command=master.quit).grid(row=3,
+                                    column=0,
+                                    sticky=tk.W,
+                                    pady=4)
+
+tk.Button(master,
+          text='Value', command=button_value).grid(row=3,
+                                                   column=2,
+                                                   sticky=tk.W,
+                                                   pady=4)
+
+tk.mainloop()
+
+#
+# root = tk.Tk()
+# def print_path():
+#     f = tkinter.filedialog.askopenfilename(
+#         parent=root, initialdir='/home/rakibul/',
+#         title='Choose file',
+#         filetypes=[('png images', '.png'),
+#                    ('gif images', '.gif')]
+#     )
+#     # image_location(f)
+#     size_input_field()
+#     return f
+#
+#
+# b1 = tkinter.Button(root, text='Print path', command=print_path)
+# b1.pack(fill='x')
+#
+# root.mainloop()
--- image_resize.py
+++ image_resize.py
@@ -1,6 +1,5 @@
-import cv2
 from datetime import datetime
-import tkinter
+import cv2
 
 
 def image_resize(src, img_height, img_weight):
@@ -13,15 +12,17 @@ def image_resize(src, img_height, img_weight):
     dsize = (height, weight)
 
     output = cv2.resize(src, dsize, interpolation=cv2.INTER_AREA)
-    nam_loc = 'test/'+d_time+'_'+str(height)+'_'+str(weight)+'.png'
+    nam_loc = 'test/' + d_time + '_' + str(height) + '_' + str(weight) + '.png'
     cv2.imwrite(nam_loc, output)
     cv2.destroyAllWindows()
 
 
-if __name__ == '__main__':
-    src = cv2.imread('python_1.png', cv2.IMREAD_UNCHANGED)
-    total_logo = int(input("Input total logo number: "))
+# if __name__ == '__main__':
+def image_location(img_src, height, weight):
+    src = cv2.imread(img_src, cv2.IMREAD_UNCHANGED)
+    # total_logo = int(input("Input total logo number: "))
 
-    for _ in range(total_logo):
-        height, weight = input().split(" ")
-        image_resize(src, int(height), int(weight))
+    # for _ in range(total_logo):
+    #     height, weight = input().split(" ")
+    #     image_resize(src, int(height), int(weight))
+    image_resize(src, int(height), int(weight))
